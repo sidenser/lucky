b183fb91a0d08e85e6dbb73d0888ea1ce0260382
Denys Simanchuk <denys.simanchuk@mpfunds.com>
2024-11-15 11:32:55 +0200

    small refactoring, add  validation

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,49 +1,54 @@
 <script setup>
-import { ref } from 'vue';
+import {defineProps, ref} from 'vue';
 import { router, usePage } from '@inertiajs/vue3'
 
 const page = usePage()
 
 const valid = ref(false);
 const formRef = ref(null)
-const isGenerate = ref(false)
 const form = ref({
     username: '',
     phone: '',
-});
+})
+
+const errors = ref()
 
 const rules = {
     required: (value) => !!value || 'This field is required',
-};
+}
 
 const submit = async () => {
 
     if (formRef.value && formRef.value.validate()) {
-        isGenerate.value = true
-
+        errors.value = null
         router.post('/registration', {
             _token: page.props.csrf_token,
             username: form.value.username,
             phone: form.value.phone,
+        },
+        {
+            onError: (errorResponse) => {
+                errors.value = errorResponse
+            },
         })
     }
 }
 </script>
 <template>
 <div>
-    <v-row v-show="isGenerate">
-        <v-col cols="12" class="text-center">
-            <div class="progress">
-                <v-progress-circular
-                    color="primary"
-                    indeterminate
-                    :size="100"
-                    :width="9"
-                ></v-progress-circular>
-            </div>
-        </v-col>
-    </v-row>
-    <v-form ref="formRef" v-show="!isGenerate" v-model="valid" lazy-validation>
+    <v-container>
+        <v-row>
+            <v-col cols="12" class="text-center">
+                <template v-for="error in errors">
+                    <v-alert
+                        color="error"
+                        :text="error"
+                    ></v-alert>
+                </template>
+            </v-col>
+        </v-row>
+    </v-container>
+    <v-form ref="formRef"  v-model="valid" lazy-validation >
         <v-container>
             <v-row>
                 <v-col cols="12" class="text-center">
